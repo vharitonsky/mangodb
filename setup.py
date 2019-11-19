327d341eeeb833dc7be29e50793426558569edb5
v.haritonskiy <v.haritonskiy@smartweb.com.ua>
2019-11-19 14:25:03 +0200

    fix classifier

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Developers',
-          'Topic :: Software Development :: DB drivers',
+          'Topic :: Software Development :: Libraries',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.6',
