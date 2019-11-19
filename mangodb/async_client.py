7e55efa98fb16e223374ccba518a7b36b9dddbb1
v.haritonskiy <v.haritonskiy@smartweb.com.ua>
2019-11-19 13:38:55 +0200

    add async client

--- mangodb/async_client.py
+++ mangodb/async_client.py
@@ -3,7 +3,7 @@ import logging
 from datetime import datetime
 
 from bson import ObjectId
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.errors import CollectionInvalid
 
 from mangodb.types import Bytes, Seconds
@@ -23,14 +23,15 @@ class DataBuffer:
         self.key = key
         self.data_getter = data_getter
 
-    def read(self, n=-1):
+    async def read(self, n=-1):
         if n == -1:
             n = sys.maxsize
         if not self.chunks:
-            chunks_doc = self.data_getter(filter={'key': self.key})
+            chunks_doc = await self.data_getter(filter={'key': self.key})
             if chunks_doc is None:
                 raise NotFoundError
             self.chunks = chunks_doc['chunks'].split()
+
         content = b''
 
         while len(content) < n:
@@ -39,7 +40,7 @@ class DataBuffer:
                     self.current_chunk = self.chunks.pop()
                 except IndexError:
                     return content + self.tail
-                doc = self.data_getter(
+                doc = await self.data_getter(
                     filter={'_id': ObjectId(self.current_chunk)}
                 )
                 if doc is None:
@@ -56,7 +57,7 @@ class DataBuffer:
 class MangoDB:
 
     def __init__(self, dsn, db, collection: str, cap: Bytes, ttl: Seconds):
-        self.client = MongoClient(dsn)
+        self.client = AsyncIOMotorClient(dsn)
         self.db = db
         self.collection = collection
         self.cap = cap
@@ -66,24 +67,24 @@ class MangoDB:
     def _read(self, **kwargs):
         return self.client[self.db][self.collection].find_one(**kwargs)
 
-    def _write_chunk(self, data):
+    async def _write_chunk(self, data):
         return str(
-            self.client[self.db][self.collection].insert_one({
+            (await self.client[self.db][self.collection].insert_one({
                 'blob': data,
                 'date': datetime.utcnow(),
-            }).inserted_id
+            })).inserted_id
         )
 
-    def _ensure_collection(self):
+    async def _ensure_collection(self):
         if self.collection_created:
             return
         try:
-            self.client[self.db].create_collection(
+            await self.client[self.db].create_collection(
                 self.collection, capped=True, size=self.cap
             )
         except CollectionInvalid:
-            collection = self.client[self.db].get_collection(self.collection)
-            if not collection.options().get('capped'):
+            options = await self.client[self.db].get_collection(self.collection).options()
+            if not options.get('capped'):
                 log.warning(
                     "Collection is not capped, perform "
                     "https://docs.mongodb.com/manual/core/capped-collections"
@@ -91,30 +92,31 @@ class MangoDB:
                 )
         self.collection_created = True
 
-    def _ensure_index(self):
-        self.client[self.db][self.collection].ensure_index(
+    async def _ensure_index(self):
+        collection = self.client[self.db][self.collection]
+        await collection.create_index(
             'date',
             name='date_expire',
             expireAfterSeconds=self.ttl
         )
 
-    def _write_chunk_ids(self, key, chunk_ids):
-        self.client[self.db][self.collection].insert_one({
+    async def _write_chunk_ids(self, key, chunk_ids):
+        await self.client[self.db][self.collection].insert_one({
             'key': key,
             'chunks': ' '.join(chunk_ids),
             'date': datetime.utcnow(),
         })
 
-    def put(self, key, buffer):
-        self._ensure_collection()
-        self._ensure_index()
+    async def put(self, key, buffer):
+        await self._ensure_collection()
+        await self._ensure_index()
         chunk_ids = []
         while True:
             chunk = buffer.read(CHUNK_SIZE)
             if not chunk:
                 break
-            chunk_ids.append(self._write_chunk(chunk))
-        self._write_chunk_ids(key, chunk_ids)
+            chunk_ids.append(await self._write_chunk(chunk))
+        await self._write_chunk_ids(key, chunk_ids)
 
     def get_buffer(self, key):
         return DataBuffer(key, self._read)
