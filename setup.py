beec82476935ddeff153641d97cec357f33d915f
v.haritonskiy <v.haritonskiy@smartweb.com.ua>
2019-11-25 21:48:55 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
       name='mangodb',
-      version='0.1',
+      version='0.2',
       description='Temporary database driver',
       url='http://github.com/vharitonsky/mangodb',
       author='Vitaliy Kharitonskiy',
